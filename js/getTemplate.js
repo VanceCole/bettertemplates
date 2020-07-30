39539a8b9ae0aa2cc6dc117d7cfdc8e87a390bd0
Vance Cole <coletain@gmail.com>
2020-07-30 00:37:09 -0700

    forgot to remove debug call

--- js/getTemplate.js
+++ js/getTemplate.js
@@ -1,6 +1,5 @@
 export default function getTemplate(id) {
   const template = canvas.templates.get(id);
   const grid = canvas.grid.getHighlightLayer(`Template.${id}`);
-  console.log(template);
   return { template, grid };
 }
